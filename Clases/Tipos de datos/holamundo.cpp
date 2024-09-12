c76590dcab8a49fe0f04707664477a7666ac8c06
Merivas <merivas@uca.edu.sv>
2024-09-12 17:02:34 -0600

    :memo: add file

---
+++ Clases/Tipos de datos/holamundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using   namespace std ;
+
+int main() {
+    int a,b ;
+   cout << "pruebaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl;
+   cout << "digite un numero porfavor " << endl;
+   cin>>a;
+   cout << "su numero que ingreso es:" + a << endl;
+
+    return 0;
+}
+
