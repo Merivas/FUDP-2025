fb98103c8e5ce36fa7fea266e05cbfd60add821b
Merivas <merivas@uca.edu.sv>
2024-09-18 14:37:01 -0600

    :rocket: add ejemplo1

---
+++ Clases/Condicional-if/ejemplo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using   namespace std ;
+
+int main() {
+    int a=2, b=2 , c=0 ,x=0;
+cout << "el valor de a es :"  << a <<endl;
+cout << "el valor de b es :" << b <<endl;
+
+c=a/2;
+x=b%2;
+
+cout << "el valor de c " << c << endl;
+cout << "el valor de x " << x << endl ;
+    return 0;
+   }
+   
