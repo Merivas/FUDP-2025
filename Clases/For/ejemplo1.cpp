02a67396750035ca8e2a519e4ad6b183e9f7a2df
Merivas <merivas@uca.edu.sv>
2024-10-02 14:53:07 -0600

    add class for

--- Clases/For/ejemplo1.cpp
+++ Clases/For/ejemplo1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main() {
-    // Ciclo for que cuenta del 1 al 10
+    int n=0, i=0;
+    cout<<i<<endl;
     for (int i = 1; i <= 10; i++) {
         // Imprimir el valor actual de i
         cout << i << " ";
