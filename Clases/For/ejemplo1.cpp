b9b3c8a1cb9d9bf863be940d58a3fe16af4d000a
Merivas <merivas@uca.edu.sv>
2024-10-02 14:31:47 -0600

    :memo: add file class while

---
+++ Clases/For/ejemplo1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Ciclo for que cuenta del 1 al 10
+    for (int i = 1; i <= 10; i++) {
+        // Imprimir el valor actual de i
+        cout << i << " ";
+    }
+
+    cout << endl;  // Salto de línea al final
+    return 0;
+}
