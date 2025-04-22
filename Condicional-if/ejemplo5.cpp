7edc2e68cab733a88d81ad3380d765e4ea412d9a
Merivas <merivas@uca.edu.sv>
2025-04-22 09:52:49 -0600

    add file :memo:

---
+++ Condicional-if/ejemplo5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using   namespace std ;
+
+int main() {
+ int numero;
+
+cout << "ingrese el numero ";
+cin >> numero;
+
+if (numero==0)
+{
+   cout<< "su numero es 0";
+}
+
+else if (numero < 0)
+
+{
+    cout << "su numero es negativo ";
+}
+
+else {
+    cout << " su numero es positivo ";
+}
+
+
+
+
+
+    return 0 ; 
+}
