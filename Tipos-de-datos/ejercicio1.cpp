4e88839a9d06f0968d1a3049de17d6da860f05c6
Merivas <merivas@uca.edu.sv>
2026-04-15 12:33:58 -0600

    acaaaaaaaaaaaaaaaaaaaa

--- Tipos-de-datos/ejercicio1.cpp
+++ Tipos-de-datos/ejercicio1.cpp
@@ -15,24 +15,27 @@ int main() {
     int diferencia = a - b;
     int producto = a * b;
     int division = a / b;
+    int division2 = a % b;
 
     // Mostrar resultados
     cout << "Suma: " << suma << endl;
-    cout << "Diferencia: " << diferencia << endl;
+    cout << "Diferencia: " << diferencia ;
     cout << "Producto: " << producto << endl;
     cout << "Division entera: " << division << endl;
+    cout << "Residuo de la division: " << division2 << endl;
 
-    // Operaciones lógicas y de comparación
+    /* Operaciones lógicas y de comparación
     bool ambosPositivos = a > 0 && b > 0;
     bool algunoMayor100 = a > 100 || b > 100;
     bool sonIguales = a == b;
     bool noDiferentes = !(a != b);
+*/
 
-
-    cout << "Ambos son positivos: " << ambosPositivos << endl;
+/* cout << "Ambos son positivos: " << ambosPositivos << endl;
     cout << "Al menos uno es mayor que 100: " << algunoMayor100 << endl;
     cout << "Los dos son iguales: " << sonIguales << endl;
     cout << "No son diferentes: " << noDiferentes << endl;
+*/
 
     return 0;
 }
